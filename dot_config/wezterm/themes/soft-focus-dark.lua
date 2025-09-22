77ac6dcb1d1d211f2612b756724f6b4936799a07
kamal-hamza <kamal.hamza@outlook.com>
2025-09-22 14:42:33 -0700

    fixed hyprland

--- dot_config/wezterm/themes/soft-focus-dark.lua
+++ dot_config/wezterm/themes/soft-focus-dark.lua
@@ -1,35 +1,29 @@
-local M = {}
-
-M.schemes = {
-    ["Soft Focus Dark"] = {
-        foreground = "#f5f5f5",
-        background = "#050505",
-        cursor_bg = "#ffffff",
-        cursor_border = "#ffffff",
-        cursor_fg = "#050505",
-        selection_bg = "#067BC2",
-        selection_fg = "#f5f5f5",
-        ansi = {
-            "#111111",
-            "#C42847",
-            "#77aa77",
-            "#D4B87B",
-            "#067BC2",
-            "#B88E8D",
-            "#67b7f5",
-            "#f5f5f5",
-        },
-        brights = {
-            "#333333",
-            "#ff4c6a",
-            "#99cc99",
-            "#f0d08a",
-            "#4da6ff",
-            "#d4a6a6",
-            "#77d5ff",
-            "#ffffff",
-        },
+return {
+    foreground = "#f5f5f5",
+    background = "#050505",
+    cursor_bg = "#ffffff",
+    cursor_border = "#ffffff",
+    cursor_fg = "#050505",
+    selection_bg = "#067BC2",
+    selection_fg = "#f5f5f5",
+    ansi = {
+        "#111111",
+        "#C42847",
+        "#77aa77",
+        "#D4B87B",
+        "#067BC2",
+        "#B88E8D",
+        "#67b7f5",
+        "#f5f5f5",
+    },
+    brights = {
+        "#333333",
+        "#ff4c6a",
+        "#99cc99",
+        "#f0d08a",
+        "#4da6ff",
+        "#d4a6a6",
+        "#77d5ff",
+        "#ffffff",
     },
 }
-
-return M
--- dot_config/wezterm/themes/soft-focus-light.lua
+++ dot_config/wezterm/themes/soft-focus-light.lua
@@ -1,35 +1,29 @@
-local M = {}
-
-M.schemes = {
-    ["Soft Focus Light"] = {
-        foreground = "#000000",
-        background = "#fbfcf8",
-        cursor_bg = "#000000",
-        cursor_border = "#000000",
-        cursor_fg = "#fbfcf8",
-        selection_bg = "#067BC2",
-        selection_fg = "#000000",
-        ansi = {
-            "#555555",
-            "#C42847",
-            "#334139",
-            "#D4B87B",
-            "#067BC2",
-            "#B88E8D",
-            "#067BC2",
-            "#000000",
-        },
-        brights = {
-            "#555555",
-            "#C42847",
-            "#334139",
-            "#D4B87B",
-            "#067BC2",
-            "#B88E8D",
-            "#067BC2",
-            "#000000",
-        },
+return {
+    foreground = "#000000",
+    background = "#fbfcf8",
+    cursor_bg = "#000000",
+    cursor_border = "#000000",
+    cursor_fg = "#fbfcf8",
+    selection_bg = "#067BC2",
+    selection_fg = "#000000",
+    ansi = {
+        "#555555",
+        "#C42847",
+        "#334139",
+        "#D4B87B",
+        "#067BC2",
+        "#B88E8D",
+        "#067BC2",
+        "#000000",
+    },
+    brights = {
+        "#555555",
+        "#C42847",
+        "#334139",
+        "#D4B87B",
+        "#067BC2",
+        "#B88E8D",
+        "#067BC2",
+        "#000000",
     },
 }
-
-return M
