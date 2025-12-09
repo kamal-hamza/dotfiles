0a9d081dc8df6281662fa0e6e63b3bf2918eaa89
kamal-hamza <kamal.hamza@outlook.com>
2025-12-09 02:48:40 -0800

    updated lsp config for nvim

--- dot_config/nvim/lua/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,13 +1,17 @@
 return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
+    cmd = { "LspInfo", "LspStart", "LspStop", "LspRestart" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
         { "antosha417/nvim-lsp-file-operations", config = true },
         { "folke/lazydev.nvim",                  ft = "lua",   opts = {} },
     },
     config = function()
         local lspconfig = require("lspconfig")
+        local mason_lspconfig = require("mason-lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local keymap = vim.keymap
 
@@ -69,111 +73,74 @@ return {
             end,
         })
 
-        -- Configure lua_ls for Neovim development
-        lspconfig["lua_ls"].setup({
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        library = {
-                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                            [vim.fn.stdpath("config") .. "/lua"] = true,
+        -- Default handlers for all servers
+        mason_lspconfig.setup_handlers({
+            -- Default handler for all servers
+            function(server_name)
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+
+            -- Custom handler for lua_ls
+            ["lua_ls"] = function()
+                lspconfig["lua_ls"].setup({
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                            workspace = {
+                                library = {
+                                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                                },
+                            },
                         },
                     },
-                },
-            },
-        })
+                })
+            end,
 
-        -- Configure TypeScript/JavaScript
-        lspconfig["ts_ls"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure HTML
-        lspconfig["html"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure CSS
-        lspconfig["cssls"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure Tailwind CSS
-        lspconfig["tailwindcss"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure Svelte
-        lspconfig["svelte"].setup({
-            capabilities = capabilities,
-            on_attach = function(client, bufnr)
-                vim.api.nvim_create_autocmd("BufWritePost", {
-                    pattern = { "*.js", "*.ts" },
-                    callback = function(ctx)
-                        client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+            -- Custom handler for svelte
+            ["svelte"] = function()
+                lspconfig["svelte"].setup({
+                    capabilities = capabilities,
+                    on_attach = function(client, bufnr)
+                        vim.api.nvim_create_autocmd("BufWritePost", {
+                            pattern = { "*.js", "*.ts" },
+                            callback = function(ctx)
+                                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                            end,
+                        })
                     end,
                 })
             end,
-        })
 
-        -- Configure GraphQL
-        lspconfig["graphql"].setup({
-            capabilities = capabilities,
-            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-        })
+            -- Custom handler for graphql
+            ["graphql"] = function()
+                lspconfig["graphql"].setup({
+                    capabilities = capabilities,
+                    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+                })
+            end,
 
-        -- Configure Emmet
-        lspconfig["emmet_ls"].setup({
-            capabilities = capabilities,
-            filetypes = {
-                "html",
-                "typescriptreact",
-                "javascriptreact",
-                "css",
-                "sass",
-                "scss",
-                "less",
-                "svelte",
-            },
-        })
-
-        -- Configure Prisma
-        lspconfig["prismals"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure Python
-        lspconfig["pyright"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure ESLint
-        lspconfig["eslint"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure JSON
-        lspconfig["jsonls"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure YAML
-        lspconfig["yamlls"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure Bash
-        lspconfig["bashls"].setup({
-            capabilities = capabilities,
-        })
-
-        -- Configure Markdown
-        lspconfig["marksman"].setup({
-            capabilities = capabilities,
+            -- Custom handler for emmet_ls
+            ["emmet_ls"] = function()
+                lspconfig["emmet_ls"].setup({
+                    capabilities = capabilities,
+                    filetypes = {
+                        "html",
+                        "typescriptreact",
+                        "javascriptreact",
+                        "css",
+                        "sass",
+                        "scss",
+                        "less",
+                        "svelte",
+                    },
+                })
+            end,
         })
     end,
 }
--- dot_config/nvim/lua/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/plugins/lsp/mason.lua
@@ -2,6 +2,7 @@ return {
     -- Mason: LSP/DAP/Linter/Formatter installer
     {
         "williamboman/mason.nvim",
+        cmd = "Mason",
         config = function()
             require("mason").setup({
                 ui = {
@@ -19,6 +20,7 @@ return {
     -- Mason LSPConfig bridge
     {
         "williamboman/mason-lspconfig.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         dependencies = {
             "williamboman/mason.nvim",
             "neovim/nvim-lspconfig",
@@ -26,21 +28,21 @@ return {
         opts = {
             -- List of servers for mason to install
             ensure_installed = {
-                "lua_ls", -- Lua
-                "ts_ls", -- TypeScript/JavaScript
-                "html", -- HTML
-                "cssls", -- CSS
+                "lua_ls",      -- Lua
+                "ts_ls",       -- TypeScript/JavaScript
+                "html",        -- HTML
+                "cssls",       -- CSS
                 "tailwindcss", -- Tailwind CSS
-                "svelte", -- Svelte
-                "graphql", -- GraphQL
-                "emmet_ls", -- Emmet
-                "prismals", -- Prisma
-                "pyright", -- Python
-                "eslint", -- ESLint
-                "jsonls", -- JSON
-                "yamlls", -- YAML
-                "bashls", -- Bash
-                "marksman", -- Markdown
+                "svelte",      -- Svelte
+                "graphql",     -- GraphQL
+                "emmet_ls",    -- Emmet
+                "prismals",    -- Prisma
+                "eslint",      -- ESLint
+                "jsonls",      -- JSON
+                "yamlls",      -- YAML
+                "bashls",      -- Bash
+                "marksman",    -- Markdown
+                "pyrefly"      -- Python
             },
             -- Auto-install configured servers (with lspconfig)
             automatic_installation = true,
@@ -50,20 +52,23 @@ return {
     -- Mason Tool Installer for formatters/linters
     {
         "WhoIsSethDaniel/mason-tool-installer.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         dependencies = { "williamboman/mason.nvim" },
         opts = {
             ensure_installed = {
                 -- Formatters
-                "prettier", -- JS/TS/JSON/YAML/Markdown/CSS/HTML
+                "prettier",  -- JS/TS/JSON/YAML/Markdown/CSS/HTML
                 "prettierd", -- Faster prettier
-                "stylua", -- Lua
-                "isort", -- Python import sorting
-                "black", -- Python
-                "shfmt", -- Shell
+                "stylua",    -- Lua
+                "ruff",      -- Python (formatting + linting)
+                "shfmt",     -- Shell
 
                 -- Linters
-                "eslint_d", -- JS/TS
-                "pylint", -- Python
+                "eslint_d",     -- JS/TS
+                "markdownlint", -- Markdown
+                "hadolint",     -- Dockerfile
+                "yamllint",     -- YAML
+                "shellcheck",   -- Shell
             },
             auto_update = false,
             run_on_start = true,
