26255c85d71fe5742e972f18b3ad4b7f93e02688
kamal-hamza <kamal.hamza@outlook.com>
2026-05-23 19:43:16 -0700

    added lock and tty binds

--- dot_config/hypr/conf/keymaps.lua
+++ dot_config/hypr/conf/keymaps.lua
@@ -1,10 +1,16 @@
 local super = "ALT"
 local terminal = "kitty"
 local fileManager = "thunar"
+local lock = "hyprlock"
 
 hl.bind(super .. " + RETURN", hl.dsp.exec_cmd(terminal))
 hl.bind(super .. " + E", hl.dsp.exec_cmd(fileManager))
 
+-- go back to tty
+hl.bind(super .. " + M", hl.dsp.exec_cmd("loginctl terminate-user \"\""))
+
+-- lock
+hl.bind(super .. " + L", hl.dsp.exec_cmd(lock))
 
 -- close any window
 hl.bind(super .. " +Q", hl.dsp.window.kill())
