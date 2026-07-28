5db445fa129431586d4ad7db0ebd5440e26c7cc5
kamal-hamza <kamal.hamza@outlook.com>
2026-07-28 01:15:52 +0000

    lazy load bufferline

--- dot_config/nvim/lua/hkamal/plugins/bufferline.lua
+++ dot_config/nvim/lua/hkamal/plugins/bufferline.lua
@@ -3,17 +3,22 @@ vim.pack.add({
     "https://github.com/nvim-tree/nvim-web-devicons",
 })
 
-require("bufferline").setup({
-    options = {
-        mode = "buffers",
-        separator_style = "thin",
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        always_show_bufferline = true,
-        offsets = {
-            { filetype = "NvimTree", text = "Explorer", highlight = "Directory", text_align = "left" },
-        },
-    },
+vim.api.nvim_create_autocmd("BufReadPre", {
+    once = true,
+    callback = function()
+        require("bufferline").setup({
+            options = {
+                mode = "buffers",
+                separator_style = "thin",
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                always_show_bufferline = true,
+                offsets = {
+                    { filetype = "NvimTree", text = "Explorer", highlight = "Directory", text_align = "left" },
+                },
+            },
+        })
+    end,
 })
 
 vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
